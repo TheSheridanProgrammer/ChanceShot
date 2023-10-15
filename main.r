ea3865c352bc988d781a8fb4618fad5b7c646756
TheSheridanProgrammer <absaintdane@gmail.com>
2023-10-15 18:53:27 -0400

    Major updates~~!!

--- main.r
+++ main.r
@@ -7,14 +7,17 @@ userInput <- readline("Please enter your name: ")
 # Start the match
 cat("Hello, ", userInput, "!\n")
 
-# Create a new player object
-person <- new("Person", name = userInput, hp = 10)
+# Define the player class
+setClass("Player", slots = list(name = "character", hp = "numeric"))
 
 # Create a new player object
-person2 <- new("Person", name = "Terminator", hp = 30)
+person <- new("Player", name = userInput, hp = 10)
+
+# Create a new player object
+person2 <- new("Player", name = "Terminator", hp = 30)
 
 # Loop to calculate damage each round
-while (person@hp < 10){
+while (person@hp <= 10){
     # Generate random number for the damage
     random_number <- sample(1:10, 1)
     person@hp = person@hp - random_number
